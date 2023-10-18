eeea597dc2cb3c32c436094e445c25540f50b4fa
amin1063 <79442688+amin1063@users.noreply.github.com>
2023-10-18 13:36:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 app.use("*",function(req,res){
-  res.sendFile.(path.join(__dirname,"./client/build/index.html"));
+  res.sendFile(path.join(__dirname,"./client/build/index.html"));
 });
 
 app.listen(port, () => {
